e8d83c29412b106c64ab401e88da5ffe9721a4f3
Brandon Withrow <withrobm@gmail.com>
2023-07-22 22:41:23 -0700

    Update Dials.jsx
    deleted dialDictionary as is unused

--- client/src/components/playComponents/Dials.jsx
+++ client/src/components/playComponents/Dials.jsx
@@ -1,62 +1,6 @@
 import React, { useEffect, useState } from "react";
 
 const Dials = ({puzzle, setPuzzle}) => {
-    const dialDictionary = {
-        1: {
-            "off_locked": ".../static/dials/1FL.png",
-            "off_unlocked": ".../static/dials/1FU.png",
-            "on_locked": ".../static/dials/1NL.png",
-            "on_unlocked": ".../static/dials/1NU.png"
-        },
-        2: {
-            "off_locked": ".../static/dials/2FL.png",
-            "off_unlocked": ".../static/dials/2FU.png",
-            "on_locked": ".../static/dials/2NL.png",
-            "on_unlocked": ".../static/dials/2NU.png"
-        },
-        3: {
-            "off_locked": ".../static/dials/3FL.png",
-            "off_unlocked": ".../static/dials/3FU.png",
-            "on_locked": ".../static/dials/3NL.png",
-            "on_unlocked": ".../static/dials/3NU.png"
-        },
-        4: {
-            "off_locked": ".../static/dials/4FL.png",
-            "off_unlocked": ".../static/dials/4FU.png",
-            "on_locked": ".../static/dials/4NL.png",
-            "on_unlocked": ".../static/dials/4NU.png"
-        },
-        5: {
-            "off_locked": ".../static/dials/5FL.png",
-            "off_unlocked": ".../static/dials/5FU.png",
-            "on_locked": ".../static/dials/5NL.png",
-            "on_unlocked": ".../static/dials/5NU.png"
-        },
-        6: {
-            "off_locked": ".../static/dials/6FL.png",
-            "off_unlocked": ".../static/dials/6FU.png",
-            "on_locked": ".../static/dials/6NL.png",
-            "on_unlocked": ".../static/dials/6NU.png"
-        },
-        7: {
-            "off_locked": ".../static/dials/7FL.png",
-            "off_unlocked": ".../static/dials/7FU.png",
-            "on_locked": ".../static/dials/7NL.png",
-            "on_unlocked": ".../static/dials/7NU.png"
-        },
-        8: {
-            "off_locked": ".../static/dials/8FL.png",
-            "off_unlocked": ".../static/dials/8FU.png",
-            "on_locked": ".../static/dials/8NL.png",
-            "on_unlocked": ".../static/dials/8NU.png"
-        },
-        9: {
-            "off_locked": ".../static/dials/9FL.png",
-            "off_unlocked": ".../static/dials/9FU.png",
-            "on_locked": ".../static/dials/9NL.png",
-            "on_unlocked": ".../static/dials/9NU.png"
-        }
-    };
 
     const [pairedLighting, setPairedLighting] = useState({
         "a": false,
