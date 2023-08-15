d39446e35874c3f814cf9a7106f9555dcf9e671f
Brandon Withrow <withrobm@gmail.com>
2023-08-15 15:29:30 -0700

    Update api.jsx

--- client/src/service/api.jsx
+++ client/src/service/api.jsx
@@ -1,1 +1,2 @@
-export const puzzleApi = "http://127.0.0.1:8000/api/puzzles";
+// export const puzzleApi = "http://127.0.0.1:8000/api/puzzles";
+export const puzzleApi = "https://deathcultpuzzles-api.onrender.com/api/puzzles";
