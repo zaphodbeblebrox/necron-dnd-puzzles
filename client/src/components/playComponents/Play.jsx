ee003838ddc5660f4e09cb7d2d50a2becac8b536
Brandon Withrow <withrobm@gmail.com>
2023-08-14 21:24:16 -0700

    Moved background audio
    This is due to browsers not allowing auto play of audio

--- client/src/components/playComponents/Play.jsx
+++ client/src/components/playComponents/Play.jsx
@@ -1,21 +1,20 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import {Link, useParams} from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Dials from "./Dials";
 import PlayHeader from "./PlayHeader";
 import Attempt from "./Attempt";
-import {socket} from "../../service/socket";
+import { socket } from "../../service/socket";
 
 const Play = (props) => {
-    const ambientAudioPath = "/static/audio/factory_ambience.mp3";
-    const {id} = useParams();
+    const { id } = useParams();
     const [isCorrect, setIsCorrect] = useState(false);
     const [puzzle, setPuzzle] = useState({
-        "current_positions": [[1]],
-        "locked_positions": [[1]],
-        "solution_positions": [[1]],
-        "paired_positions": [[1]],
-        "default_positions": [[1]],
+        current_positions: [[1]],
+        locked_positions: [[1]],
+        solution_positions: [[1]],
+        paired_positions: [[1]],
+        default_positions: [[1]],
     });
 
     // const socket = io("http://localhost:8000/")
@@ -24,45 +23,34 @@ const Play = (props) => {
     //     socket.join(id)
     // })
 
-    
-
     useEffect(() => {
-        socket.emit("join-room", id, updater => {
-            
-        });
+        socket.emit("join-room", id, (updater) => {});
 
-        axios.get(`http://127.0.0.1:8000/api/puzzles/${id}`)
-            .then(res => {
+        axios
+            .get(`http://127.0.0.1:8000/api/puzzles/${id}`)
+            .then((res) => {
                 // console.log(res.data);
-                const newPuzzle = {...res.data};
+                const newPuzzle = { ...res.data };
                 newPuzzle.current_positions = res.data.default_positions.map((row) => [...row]);
                 setPuzzle(newPuzzle);
                 // console.log(newPuzzle);
             })
-            .catch(err => console.log("Error getting the Puzzle",err));
-
-        // Play audio
-        const audioElement = document.getElementById('audio-ambient');
-        //audioElement.play();
-    }, [])
+            .catch((err) => console.log("Error getting the Puzzle", err));
+    }, []);
 
     // useEffect(() => console.log(puzzle),[puzzle]);
 
-    return(
+    return (
         <div>
             <div>
                 <PlayHeader />
             </div>
             <div className="d-flex flex-row justify-content-center">
-                <Dials puzzle={puzzle} setPuzzle={setPuzzle} isCorrect={isCorrect} roomId={id}/>
-                <Attempt puzzle={puzzle} setIsCorrect={setIsCorrect}/>
-                <audio id="audio-ambient" controls hidden loop>
-                    <source src={ambientAudioPath} type="audio/mp3" />
-                </audio>
-
+                <Dials puzzle={puzzle} setPuzzle={setPuzzle} isCorrect={isCorrect} roomId={id} />
+                <Attempt puzzle={puzzle} setIsCorrect={setIsCorrect} />
             </div>
         </div>
     );
-}
+};
 
 export default Play;
--- client/src/components/playComponents/PlayHeader.jsx
+++ client/src/components/playComponents/PlayHeader.jsx
@@ -1,14 +1,36 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import {Link, useParams} from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const PlayHeader = (props) => {
+    const ambientAudioPath = "/static/audio/factory_ambience.mp3";
+    const [audioState, setAudioState] = useState(false);
 
-    return(
-        <div className="d-flex justify-content-start">
-            <Link className="btn btn-primary btn-outline-dark margin-5" to="/puzzles">Home</Link>
+    const playAudioHandler = () => {
+        // Play audio
+        const audioElement = document.getElementById("audio-ambient");
+        if (audioElement.paused) {
+            audioElement.play();
+            setAudioState(true);
+        } else {
+            audioElement.pause();
+            setAudioState(false);
+        }
+    };
+
+    return (
+        <div className="d-flex justify-content-space-start">
+            <Link className="btn btn-primary btn-outline-dark margin-5" to="/puzzles">
+                Home
+            </Link>
+            <button className="btn btn-primary btn-outline-dark margin-5" onClick={playAudioHandler}>
+                {audioState ? "Pause" : "Play"}
+            </button>
+            <audio id="audio-ambient" controls hidden loop>
+                <source src={ambientAudioPath} type="audio/mp3" />
+            </audio>
         </div>
     );
-}
+};
 
 export default PlayHeader;
